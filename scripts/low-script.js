93de4b061e3a384fd1f0959f5204cdc044e4d2f2
Yothetechie <yothetechie@gmail.com>
2024-09-08 17:47:50 +0530

    first commit

---
+++ scripts/low-script.js
@@ -0,0 +1,8 @@
+// JavaScript to toggle the menu
+const btn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
+
+btn.addEventListener('click', () => {
+    menu.classList.toggle('hidden');
+    btn.classList.toggle('open');
+});
